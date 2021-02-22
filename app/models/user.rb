5923d75f410a05207525e6a48e2c19fa29ca1fd8
sadmanahmed <abirsadmanahmed177@gmail.com>
2021-02-22 07:34:55 +0600

    Add uniquness constraint for the username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username , presence: true , length: {maximum: 15, minimum: 3}
+  validates :username , presence: true , length: {maximum: 15, minimum: 3},uniqueness: { case_sensitive: false }
   has_secure_password
 
 end
