1911e7afea5d06bbd30cbce11bd4406f80939c18
sadmanahmed <abirsadmanahmed177@gmail.com>
2021-02-23 01:23:08 +0600

    Add message resources

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+
+  has_many :messages
   validates :username , presence: true , length: {maximum: 15, minimum: 3},uniqueness: { case_sensitive: false }
+
   has_secure_password
 
 end
