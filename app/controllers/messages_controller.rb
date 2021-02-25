010fe7b571e6f73ebe0db152c65c1920b96aee0c
sadmanahmed <abirsadmanahmed177@gmail.com>
2021-02-25 10:00:19 +0600

    Display real time messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,14 +5,22 @@ class MessagesController < ApplicationController
     message = current_user.messages.build(message_params)
     if message.save
       ActionCable.server.broadcast "chatroom_channel",
-                                    foo: message.body
+                                    mod_message: message_render(message)
     end
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path
   end
 
   private
 
   def message_params
     params.require(:message).permit(:body)
+  end
+  def message_render(message)
+    render(partial: 'message', locals: {message: message})
   end
 
 end
